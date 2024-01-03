e9527c7de7072daf82e7ff61de74aab7af204e7a
Abiram <uthayakumarabiram@gmail.com>
2024-01-04 05:08:40 +0530

    2 commit

---
+++ frontend/src/TodoItem.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+//Add API, we ll need it later when we send a delete request
+const API_BASE= 'http://localhost:4001/todo';
+function TodoItem(props){
+  const {name, id, completed, setItems} = props
+ 
+    const deleteTodo = async(id) => {
+      try{
+          const response = await fetch(API_BASE + "/delete/" + id, {
+              method: "DELETE",
+            });
+          if(!response.ok){
+              throw new Error("Faild to delete a task")
+          } 
+          const data = await response.json()
+          setItems(items=> items.filter(item=> item._id !== data._id))
+      }catch (error) {
+          console.error("Error updating task status:", error);
+        }
+    }
+
+  return(
+   <div className={"todo" + (completed ? " check-complete" : "")} key={id}>
+      <div className="text">{name}</div>
+      <div className="delete-todo" onClick={()=>deleteTodo(id)}><span >X</span></div>
+    </div>
+    
+  )
+}
+
+export default TodoItem;
