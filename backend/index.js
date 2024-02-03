064f8067d67b56f36d85c43e9f9eaf08258b98d0
Abiram <uthayakumarabiram@gmail.com>
2024-02-03 16:03:08 +0530

    hope1

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
-import express from 'express'; 
-import cors  from'cors'; 
-import mongoose from 'mongoose'; 
-import dotenv from "dotenv"
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 dotenv.config(); 
 import {Todo} from'./models/todo.js';
 
