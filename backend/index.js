82767c6412d2979b10cc3dbe1fe5a635da65b5da
Abiram <uthayakumarabiram@gmail.com>
2024-02-09 06:40:31 +0530

    hoaefwrqfsaf

--- backend/index.js
+++ backend/index.js
@@ -19,17 +19,17 @@ mongoose.connect(connectionString)
 .catch((err) => console.error('Connection error:', err));
 app.use(express.json()); app.use(express.urlencoded({ extended: true }));
 //Routes 
-app.get('/todo', async (req, res) => { 
+app.get('/', async (req, res) => { 
    const allTasks = await Todo.find();
    res.json(allTasks)
  });
 
-app.post('/todo/new', async (req,res) => {
+app.post('/new', async (req,res) => {
     const newTask = await Todo.create(req.body);
     res.status(201).json({newTask})
 })
 
-app.delete('/todo/delete/:id', async(req,res)=>{
+app.delete('/delete/:id', async(req,res)=>{
     const result = await Todo.findByIdAndDelete(req.params.id)
     res.json(result)
 })
